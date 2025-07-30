f675749eb9bf0d7aa9af48572c25f372f93ccab1
andecodelima <andrelimag89@gmail.com>
2025-07-30 14:26:48 -0300

    feat(categoria e CT): add endpoints

--- workspace-venv/workout_api/categoria/categoria_controller.py
+++ workspace-venv/workout_api/categoria/categoria_controller.py
@@ -8,6 +8,29 @@ from fastapi import HTTPException
 
 router = APIRouter()
 
+# INSERT
+@router.post(
+    path='/',
+    summary='Criar nova Categoria',
+    status_code=status.HTTP_201_CREATED,
+    response_model=CategoriaSchemaOut
+)
+async def Post(
+    db_session: DatabaseDependency,
+    CategoriaSchemaIn: CategoriaSchemaIn = Body()
+) -> CategoriaSchemaOut: # type: ignore
+    
+    categoriaOut = CategoriaSchemaOut(id=uuid4(), ** CategoriaSchemaIn.model_dump())
+    categoriaModel = CategoriaModel(**categoriaOut.model_dump())
+    
+    db_session.add(categoriaModel)
+    await db_session.commit()
+    await db_session.refresh(categoriaModel)
+    
+    return categoriaOut
+
+
+# GET ALL
 @router.get(
     path='/',
     summary='Consultar todas as categorias',
@@ -23,3 +46,23 @@ async def query(db_session: DatabaseDependency) -> list[CategoriaSchemaOut]:  # 
     categorias_out = [CategoriaSchemaOut.model_validate(categoria) for categoria in categorias_model]
     
     return categorias_out   
+
+# GET BY ID
+@router.get(
+    path='/{id}',
+    summary='Consutar Categoria pelo Id',
+    status_code=status.HTTP_404_NOT_FOUND,
+    response_model=CategoriaSchemaOut
+)
+async def query(id: UUID, db_session:DatabaseDependency) -> CategoriaSchemaOut: #type: ignore
+    
+    categoria: CategoriaSchemaOut = (await db_session.execute(select(CategoriaModel).filter_by(id = id))).scalars().first() #type: ignore
+     
+    if not categoria: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Categoria não encontrada para o id {id}'
+        )   
+        
+    return categoria
+    
--- workspace-venv/workout_api/centro_treinamento/centro_treinamento_controller.py
+++ workspace-venv/workout_api/centro_treinamento/centro_treinamento_controller.py
@@ -1,14 +1,16 @@
 
-from uuid import uuid4
+from uuid import UUID, uuid4
 from fastapi import APIRouter, Body, status
 from sqlalchemy import select
 from workout_api.centro_treinamento.centro_treinamento_schema import CentroTreinamentoSchemaOut
 from workout_api.contrib.dependencies import DatabaseDependency
 from workout_api.centro_treinamento.centro_treinamento_schema import CentroTreinamentoSchemaIn
 from workout_api.centro_treinamento.centro_treinamento_model import CentroTreinamentoModel
+from fastapi import HTTPException
 
 router = APIRouter()
 
+# INSERT
 @router.post(
     path='/',
     summary='Criar novo CT',
@@ -30,6 +32,7 @@ async def Post(
     return centroTreinamentoOut
 
 
+# GET ALL
 @router.get(
     path='/',
     summary='Consultar todos os CTs',
@@ -42,3 +45,22 @@ async def query(db_session:DatabaseDependency)-> list[CentroTreinamentoSchemaOut
     centroTreinamentoOut = [CentroTreinamentoSchemaOut.model_validate(categoria) for categoria in centroTreinamentoModel]
     
     return centroTreinamentoOut
+
+# GET BY ID
+@router.get(
+    path='/{id}',
+    summary='Consultar CT pelo ID',
+    status_code=status.HTTP_200_OK,
+    response_model=CentroTreinamentoSchemaOut
+    
+)
+async def query(id: UUID, db_session:DatabaseDependency) -> CentroTreinamentoSchemaOut: # type: ignore
+
+    centro_treinamento: CentroTreinamentoSchemaOut = (await db_session.execute(select(CentroTreinamentoModel).filter_by(id = id))).scalars().first() #type: ignore
+    
+    if not centro_treinamento:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f'CT não encontrado para o id {id}'
+        )
+    return centro_treinamento
